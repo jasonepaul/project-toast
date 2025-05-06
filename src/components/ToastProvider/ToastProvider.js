c4cce6c9b2b92fd89e7e468d68024c9ab85c3677
Jason Paul <jpaul@pason.com>
2025-05-06 09:46:15 -0600

    Keyboard and screen reader support

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -5,6 +5,20 @@ export const ToastContext = React.createContext();
 function ToastProvider({ children }) {
   const [toasts, setToasts] = React.useState([]);
 
+  React.useEffect(() => {
+    function handleKeyDown(event) {
+      if (event.code === 'Escape') {
+        setToasts([]);
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
   const handleAddToast = React.useCallback(
     (message, variant) => {
       const newToast = { message, variant, key: crypto.randomUUID() };
@@ -25,6 +39,7 @@ function ToastProvider({ children }) {
   const toastValues = React.useMemo(() => {
     return {
       toasts,
+      setToasts,
       handleAddToast,
       handleRemoveToast,
     };
